17963b2a327996b293806d2fd2ed5aa831bff364
stefanarctic <aleluianu09@gmail.com>
2023-02-05 13:02:49 +0200

    Made Changes

---
+++ Assets/Scripts/PageManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageManager : MonoBehaviour
+{
+
+    public MenuScript menuScript;
+    public GameObject storyMenu;
+    public GameObject background;
+    public GameObject currentPage;
+    public GameObject[] pages;
+
+    public int pageNumber;
+
+    public void Init()
+    {
+        storyMenu.SetActive(true);
+        pageNumber = 1;
+        currentPage = pages[0];
+        currentPage.SetActive(true);
+    }
+
+    public void NextPage()
+    {
+        currentPage.SetActive(false);
+        currentPage = pages[++pageNumber - 1];
+        currentPage.SetActive(true);
+    }
+
+    public void Page1Next()
+    {
+        //print("Page1Next before next page");
+        NextPage();
+        //print("Page1Next after next page");
+    }
+
+    public void Page2Next()
+    {
+        NextPage();
+        //background.SetActive(false);
+    }
+
+    public void Page3Next()
+    {
+        NextPage();
+    }
+
+    public void Page4Next()
+    {
+        //go to pyramid
+        storyMenu.SetActive(false);
+        menuScript.OnPlay();
+    }
+
+}
