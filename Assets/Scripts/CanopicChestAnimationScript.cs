44a1aff45b4e13e5351b393274ea3b7eddb0267e
stefanarctic <aleluianu09@gmail.com>
2023-02-11 11:21:31 +0200

    Changes

--- Assets/Scripts/CanopicChestAnimationScript.cs
+++ Assets/Scripts/CanopicChestAnimationScript.cs
@@ -6,10 +6,39 @@ public class CanopicChestAnimationScript : MonoBehaviour
 {
 
     public Animator animator;
+    public GameObject crosshair;
+    public GameObject player;
+    public float maxDistance = 8f;
 
-    private void Start()
+    private bool startedAnimation = false;
+
+    private void OnMouseOver()
     {
-        animator.SetTrigger("OnPlay");
+        //print("On mouse over");
+        ShowCrosshair();
+        if(!startedAnimation
+            && Input.GetMouseButtonDown(0) && Vector3.Distance(player.transform.position, gameObject.transform.position) < maxDistance)
+        {
+            startedAnimation = true;
+            animator.SetTrigger("OnPlay");
+            print("Triggered animation");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        HideCrosshair();
+    }
+
+    public void ShowCrosshair()
+    {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < maxDistance)
+            crosshair.SetActive(true);
+    }
+
+    public void HideCrosshair()
+    {
+        crosshair.SetActive(false);
     }
 
 }
