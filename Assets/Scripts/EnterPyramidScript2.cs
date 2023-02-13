9bd6632f88f6dd444afbf438e22b91fe6121c915
stefanarctic <aleluianu09@gmail.com>
2023-02-13 11:47:12 +0200

    Tweaks

--- Assets/Scripts/EnterPyramidScript2.cs
+++ Assets/Scripts/EnterPyramidScript2.cs
@@ -21,6 +21,7 @@ public class EnterPyramidScript2 : MonoBehaviour
             print($"Collision with {collision.gameObject.name}");
             //Destroy(col); 
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+            MenuScript.instance.ShowMenu();
         }
     }
 }
